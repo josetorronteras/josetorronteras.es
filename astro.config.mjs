f6df1bea8f9cb85b29929824632f56e9a55adbd0
Jose Torronteras <7280807+josetorronteras@users.noreply.github.com>
2025-05-28 17:55:17 +0200

    feat(header): implement header and responsive navigation with mobile menu (#7)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://josetorronteras.es",
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
