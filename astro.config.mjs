c2661169b096f22f564d8fa21e6bb81dcae82b4b
Jose Torronteras <7280807+josetorronteras@users.noreply.github.com>
2026-03-27 12:21:47 +0100

    fix: astro config remote image

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: "https://josetorronteras.es",
+  image: {
+    domains: ["avatars.githubusercontent.com"],
+  },
   vite: {
     plugins: [tailwindcss()],
   },
